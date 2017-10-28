1a9ad5980e2c622d3b206d12d9bbd8c25eec0746
fpo <fpo@adorsys.com>
2017-10-28 17:33:39 +0200

    initial version

---
+++ install-keycloak-with-oracle.sh
@@ -0,0 +1,33 @@
+export KEYCLOAK_VERSION=3.3.0.Final
+
+export JBOSS_HOME=keycloak-$KEYCLOAK_VERSION
+
+if [ -d "$JBOSS_HOME" ]; then
+  	rm -rf $JBOSS_HOME
+   	sleep 1s	
+fi
+
+
+export KEYCLOAK_DIST=keycloak-$KEYCLOAK_VERSION.tar.gz
+if [ ! -e "$KEYCLOAK_DIST" ]; then
+   	echo " KEYCLOAK_DIST: $KEYCLOAK_DIST not yet downloaded. Dowloading dist"
+   	curl -O https://downloads.jboss.org/keycloak/$KEYCLOAK_VERSION/keycloak-$KEYCLOAK_VERSION.tar.gz	
+else
+	echo " KEYCLOAK_DIST: $KEYCLOAK_DIST"
+fi
+
+# Unpack naked keycloak
+tar xzf $KEYCLOAK_DIST
+
+# Install oracle jdbc driver
+mkdir -p $JBOSS_HOME/modules/system/layers/base/com/oracle/ojdbc6/main
+cp ojdbc6-11.2.0.3.jar $JBOSS_HOME/modules/system/layers/base/com/oracle/ojdbc6/main/
+cp module.xml $JBOSS_HOME/modules/system/layers/base/com/oracle/ojdbc6/main/
+
+$JBOSS_HOME/bin/jboss-cli.sh --file=standalone-configuration.cli
+
+# export ORACLE_TCP_ADDR=oracle
+# export ORACLE_DATABASE=xe
+
+
+
